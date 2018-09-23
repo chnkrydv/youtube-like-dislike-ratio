6a7c8ef5bb9d9cfe19906d6842948c76c69e5554
Chandan Kumar <chn291289@gmail.com>
2018-09-23 19:39:57 +0530

    added mutation observers to fix 'not loading at first time' bug, probably complete first version

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+console.log('at least it loaded when youtube loaded')
 function addMaterialLink() {
   var link = document.createElement('link');
   link.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
@@ -6,10 +7,10 @@ function addMaterialLink() {
   document.head.appendChild(link);
 }
 
-function addStyle(){
+function addStyleToHead() {
   var style = document.createElement('style');
-  var browserSpecificBarThickness = function(){
-    if(navigator.userAgent.indexOf("Firefox") != -1 )  return `border-bottom: 2px solid #888;`;
+  var browserSpecificBarThickness = function () {
+    if (navigator.userAgent.indexOf("Firefox") != -1) return `border-bottom: 2px solid #888;`;
 
     return `border-bottom: 2.5px solid #888;`;
   }
@@ -44,50 +45,166 @@ function addStyle(){
   document.head.appendChild(style);
 }
 
-function getSentimentCount(){
-  likesCount = 1;
-  dislikesCount = 1;
-  var likes = Array.from(document.getElementsByTagName('yt-formatted-string'));
-  likes.forEach( like => {
-    var aria = like.getAttribute('aria-label');
-    if(!aria) return;
-    
-    var ariaPhrase = aria && aria.split(' ');
-    var firstWord = ariaPhrase.length && +ariaPhrase[0].split(',').join('');
-    var count = isNaN(firstWord) ? 1 : firstWord;
+function getSentimentNodes(){
+  var likes = null;
+  var dislikes = null;
+  var similar = Array.from(document.getElementsByTagName('yt-formatted-string'));
 
-    if(ariaPhrase && ariaPhrase.includes('likes')) likesCount = count;
-    if(ariaPhrase && ariaPhrase.includes('dislikes')) dislikesCount = count;
+  similar.forEach(node => {
+    var ariaPhrase = node.getAttribute('aria-label');
+    if (!ariaPhrase) return;
+
+    var ariaPhraseArr = ariaPhrase && ariaPhrase.split(' ');
+
+    if (ariaPhraseArr && (ariaPhraseArr.includes('likes') || ariaPhraseArr.includes('like'))) likes = node;
+    if (ariaPhraseArr && (ariaPhraseArr.includes('dislikes') || ariaPhraseArr.includes('dislike'))) dislikes = node;
   });
 
-  if(dislikesCount > likesCount) return '1/' + Math.floor(dislikesCount/likesCount);
+  return likes && dislikes && { likes, dislikes };
+}
+
+function getCountFromPhrase(phrase){
+  var phraseArr = phrase.split(' ');
+  var firstWord = phraseArr.length && +phraseArr[0].split(',').join('');
+  
+  return isNaN(firstWord) ? 1 : firstWord;
+}
+
+function getSentimentCount() {
+  var sentiments = getSentimentNodes();
+  if(!sentiments) return console.log('returning as no sentiment nodes detected as of') || 0.0;
+
+  var likesAria = sentiments.likes.getAttribute('aria-label');
+  var dislikesAria = sentiments.dislikes.getAttribute('aria-label');
+
+  var likesCount = getCountFromPhrase(likesAria) || 1;
+  var dislikesCount = getCountFromPhrase(dislikesAria) || 1;
+
+  if (dislikesCount > likesCount) return '1/' + Math.floor(dislikesCount / likesCount);
 
   return Math.floor(likesCount / dislikesCount);
 }
 
-function addUI() {
-  var flex = document.getElementById('flex');
+function createUI() {
+  var ratioCount = getSentimentCount();
   var container = document.createElement('span');
   var icon = document.createElement('i');
   var ratio = document.createElement('span');
-  
-  flex.classList.add('ratio-container');
+
+  container.setAttribute('id', 'yt-like-dislike-ratio')
   container.classList.add('like-dislike-ratio');
-  icon.classList.add('material-icons','like-dislike-icon');
+  icon.classList.add('material-icons', 'like-dislike-icon');
 
   icon.innerText = 'thumbs_up_down';
-  ratio.innerText = '' + getSentimentCount();
+  ratio.innerText = isNaN(ratioCount) ? '' : ratioCount.toString();
 
   container.appendChild(icon);
   container.appendChild(ratio);
 
-  flex.appendChild(container);
+  return container;
+}
+
+function deleteUI(ui) {
+  ui.parentNode.removeChild(ui);
+}
+
+function updateUI() {
+  var ui = document.getElementById('yt-like-dislike-ratio');
+  if (ui) deleteUI(ui);
+
+  var flex = document.getElementById('flex');
+  if(!flex) return;
+  flex.classList.add('ratio-container');
+
+  flex.appendChild(createUI());
+}
+
+function observeNode(node, callback){
+  var config = { attributes: true, childList: true, subtree: true };
+  
+  var observer = new MutationObserver(callback);
+  observer.observe(node, config);
+  console.log(node.tagName + ' observer started');
 }
 
 function loadExtension() {
   addMaterialLink();
-  addStyle();
-  addUI();
+  addStyleToHead();
+  runUiUpdater();
+  console.log('called observer');
 }
 
-setTimeout(loadExtension, 500);
+function runUiUpdater(){
+  console.log('ui updater invoked to run')
+  observeNode(document.body, observeSentimentsAfterFinding)
+}
+
+function observeSentimentsAfterFinding(documentBodyMutations, bodyObserver){
+  for (var mutation of documentBodyMutations) {
+    if (mutation.type == 'attributes') {
+      if(getSentimentNodes()) {
+        bodyObserver.disconnect(console.log('disconnect triggered'));
+  
+        startObservingSentiments();
+        break;
+      }
+    }
+  }
+}
+
+function startObservingSentiments(){
+  console.log('started observing sentiments');
+  var sentimentNodes = getSentimentNodes();
+  updateUI();
+  if(sentimentNodes){
+    observeNode(sentimentNodes.likes, updateSentiments);
+    observeNode(sentimentNodes.dislikes, updateSentiments);
+  }
+}
+
+function updateSentiments(sentimentMutationsList){
+  for (var mutation of sentimentMutationsList) {
+    if (mutation.type == 'attributes' && mutation.attributeName === "aria-label") {
+      updateUI();
+    }
+  }
+}
+
+setTimeout(loadExtension, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+// function observeAndUpdateUI(node) {
+//   console.log('observer code reached');
+//   var targetNode = node || document.body;
+//   console.log(targetNode);
+//   var config = { attributes: true, childList: true, subtree: true };
+
+//   var callback = function (mutationsList, observer) {
+//     if(targetNode === document.body){
+//       console.log('still observing document body')
+//     }
+//     for (var mutation of mutationsList) {
+//       if (mutation.type == 'attributes') {
+//         if(getSentimentNodes()) {
+//           observer.disconnect();
+//           updateUI();
+//         }
+//         console.log(targetNode);
+//       }
+//     }
+//   };
+  
+//   var observer = new MutationObserver(callback);
+//   observer.observe(targetNode, config);
+//   console.log('observer started');
+// }
