f7b78457467708c94b5d8408aecf76aff27f6587
Chandan Kumar <chn291289@gmail.com>
2021-05-24 18:59:11 +0530

    [v2.0] bugs fixed

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var RENDERED_ELEMENT_ID = 'yt-like-dislike-ratio'
 var RENDERED_ELEMENT_PARENT_CLASS = 'yt-like-dislike-ratio'
 var RENDERED_ELEMENT_PARENT_QUERY_SELECTOR = '#info #flex'
 var SENTIMENTS_TOOLTIP_NODE_QUERY_SELECTOR = 'ytd-sentiment-bar-renderer #tooltip'
+var LIKES_COUNT_NODE_QUERY_SELECTOR = 'ytd-menu-renderer yt-formatted-string'
 
 // END: Constants
 
@@ -45,52 +46,30 @@ function addStyleToHead() {
 
 
 // START: finding and getting sentiment nodes and calculating counts from them
-function getSentimentNodes(){
-  var likes = null;
-  var dislikes = null;
-  var similar = Array.from(document.getElementsByTagName('yt-formatted-string'));
-
-  similar.forEach(node => {
-    var ariaPhrase = node.getAttribute('aria-label');
-    if (!ariaPhrase) return;
-
-    var ariaPhraseArr = ariaPhrase && ariaPhrase.split(' ');
-
-    if (ariaPhraseArr && (ariaPhraseArr.includes('likes') || ariaPhraseArr.includes('like'))) likes = node;
-    if (ariaPhraseArr && (ariaPhraseArr.includes('dislikes') || ariaPhraseArr.includes('dislike'))) dislikes = node;
-  });
-
-  return likes && dislikes && { likes, dislikes };
-}
-
 function getNumFromString(numString){
   return +numString.replace(/[,.\s]/g, '')
 }
 
 function getRatioString(likes, dislikes){
-  var l2dRatio = (likes/dislikes).toFixed()
-  var d2lRatio = (likes/dislikes).toFixed()
   if(!likes || !dislikes) return `${likes} : ${dislikes}`
-  else if(likes < dislikes) return `1 : ${d2lRatio}`
+
+  var l2dRatio = (likes/dislikes).toFixed()
+  var d2lRatio = (dislikes/likes).toFixed()
+  if(likes < dislikes) return `1 : ${d2lRatio}`
   else return `${l2dRatio} : 1`
 }
 
-function getSentimentsTooltipNode(){
-  return document.querySelector(SENTIMENTS_TOOLTIP_NODE_QUERY_SELECTOR)
-}
-
 function getRatioFromSentimentsTooltip(setimentsTooltipNode) {
-  console.log(setimentsTooltipNode)
-  console.log(setimentsTooltipNode.innerText.trim())
+  var sentimentsTooltipText = setimentsTooltipNode.innerText.trim()
+  if(!sentimentsTooltipText) return ''
 
-  var sentimentsTextArr = setimentsTooltipNode.innerText.trim().split('/')
-  var likesStr = sentimentsTextArr[0]
-  var dislikesStr = sentimentsTextArr[1]
+  var sentimentsTooltipTextArr = sentimentsTooltipText.split('/')
+  var likesStr = sentimentsTooltipTextArr[0]
+  var dislikesStr = sentimentsTooltipTextArr[1]
   var likesCount = getNumFromString(likesStr);
   var dislikesCount = getNumFromString(dislikesStr);
 
   var ratioString = getRatioString(likesCount, dislikesCount)
-  console.log(ratioString)
 
   return ratioString
 }
@@ -103,6 +82,8 @@ function getRatioFromSentimentsTooltip(setimentsTooltipNode) {
 function createAndGetUINode(setimentsTooltipNode) {
   var uiElementNode = document.createElement('span')
   var ratioString = getRatioFromSentimentsTooltip(setimentsTooltipNode)
+
+  if(!ratioString) return
   
   uiElementNode.setAttribute('id', RENDERED_ELEMENT_ID)
   uiElementNode.innerText = ratioString
@@ -116,11 +97,13 @@ function flushStaleUI(){
 }
 
 function updateRenderedUI(setimentsTooltipNode) {
+  flushStaleUI()
+
   var updatedUINode = createAndGetUINode(setimentsTooltipNode)
+  if(!updatedUINode) return
   var parentNode = document.querySelector(RENDERED_ELEMENT_PARENT_QUERY_SELECTOR)
   if(!parentNode) return
   
-  flushStaleUI()
   parentNode.classList.add(RENDERED_ELEMENT_PARENT_CLASS)
   parentNode.appendChild(updatedUINode);
 }
@@ -131,20 +114,33 @@ function updateRenderedUI(setimentsTooltipNode) {
 
 // START: observer logic and using it to find and observe sentiment nodes when YouTube page loads
 function triggerCallbackOnNodeMutation(node, callback){
+  if(!(node instanceof Node)) return
+
   var config = { attributes: true, childList: true, subtree: true };
-  
   var observer = new MutationObserver(callback);
   observer.observe(node, config);
+}
+
+function startObservingSentiments(setimentsTooltipNode){
+  var likesCountNode = document.querySelector(LIKES_COUNT_NODE_QUERY_SELECTOR)
+  updateRenderedUI(setimentsTooltipNode)
+  triggerCallbackOnNodeMutation(likesCountNode, function(likesCountNodeMutations){
+    for (var mutation of likesCountNodeMutations) {
+      if (mutation.type == 'attributes' && mutation.attributeName === "aria-label") {
+        updateRenderedUI(setimentsTooltipNode)
+      }
+    }
+  });
 }
 
 function findAndObserveSentimentsNode(documentBodyMutations, documentBodyObserver){
   for (var mutation of documentBodyMutations) {
     if (mutation.type == 'attributes') {
-      var setimentsTooltipNode = getSentimentsTooltipNode()
+      var setimentsTooltipNode = document.querySelector(SENTIMENTS_TOOLTIP_NODE_QUERY_SELECTOR)
       if(setimentsTooltipNode) {
         documentBodyObserver.disconnect();
   
-        updateRenderedUI(setimentsTooltipNode);
+        startObservingSentiments(setimentsTooltipNode);
         break;
       }
     }
